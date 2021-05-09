fc636640a78a466c7fc0d56b66d0c15211b7760f
Ashtyn Ce <ashtyncza@gmail.com>
2021-05-08 23:03:02 -0400

    ketchup

---
+++ my-react-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+
+    <div id="formStuff">
+      <form name="contact">
+        <input type="text" id="cheeseburgers" name="name" className="input1" placeholder="First & Last Name" name="Name"></input>
+        <input type="text" id="cheeseburgers" className="input2" placeholder="E-mail Address" name="Email Address"></input>
+        <input type="text" id="formMessage" name="place" rows="3" placeholder="Where do you want to go?" name="Message"></input>
+        <input name="name" type="submit" value="Send" class="sendButton" id="formButton"></input>
+      </form>
+    </div>
+  );
+}
+
+export default App;
