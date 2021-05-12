b07ecd11ce1286a0d2335bc29058d675f2f1f351
Ashtyn Ce <ashtyncza@gmail.com>
2021-05-11 22:41:51 -0400

    css

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -6,9 +6,9 @@ function App() {
 
     <div id="formStuff">
       <form name="contact">
-        <input type="text" id="cheeseburgers" name="name" className="input1" placeholder="First & Last Name" name="Name"></input>
+        <input type="text" id="cheeseburgers" name="name" className="input1" placeholder="Name" name="Name"></input>
         <input type="text" id="cheeseburgers" className="input2" placeholder="E-mail Address" name="Email Address"></input>
-        <input type="text" id="formMessage" name="place" rows="3" placeholder="Where do you want to go?" name="Message"></input>
+        <input type="text" id="formMessage" name="place" rows="3" placeholder="Message" name="Message"></input>
         <input name="name" type="submit" value="Send" class="sendButton" id="formButton"></input>
       </form>
     </div>
