1e6da56be1cc5fdf9fa036f130ac802840617311
Ashtyn Ce <ashtyncza@gmail.com>
2021-05-28 23:35:36 -0400

    homebody

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import Home from './Home';
 
 function App() {
   return (
---
+++ my-react-app/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home(props) {
+
+    return (
+        <div>
+            <img id="malibu" src="my-react-app/public/malibu19.png" />
+        </div>
+    )
+}
+
+export default Home;
