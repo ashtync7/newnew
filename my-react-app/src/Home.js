fd61595ed7d6e9f2dff9fd84efb135385e7e385c
Ashtyn Ce <ashtyncza@gmail.com>
2021-05-29 23:33:57 -0400

    idk

--- my-react-app/src/Home.js
+++ my-react-app/src/Home.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Home(props) {
 
     return (
-        <div>
-            <img id="malibu" src="my-react-app/public/malibu19.png" />
+        <div className="homepage">
+            <img id="malibu" src="my-react-app/public/malibu19.png" alt="too much pasta" />
         </div>
     )
 }
