bd210a447d9d6e18868a0b05e36b40d44ceda648
Ashtyn Ce <ashtyncza@gmail.com>
2021-06-04 23:46:53 -0400

    oh and

--- my-react-app/src/Pros.js
+++ my-react-app/src/Pros.js
@@ -4,11 +4,21 @@ function Pros(props) {
 
     return (
         <div>
-            <section>
-                <h2>Technical Skills</h2>
-                <p>JavaScript, React.js, Express.js, Node.js, HTML, CSS, SCSS, MongoDB,
-                AJAX, jQuery, Bootstrap, Git, GitHub, InDesign, Illustrator, Photoshop,
+            <section className="skillsOne">
+                <div>
+                    <h2>Technical Skills</h2>
+                    <p>JavaScript, React.js, Express.js, Node.js, HTML, CSS, SCSS, MongoDB,
+                    AJAX, jQuery, Bootstrap, Git, GitHub, InDesign, Illustrator, Photoshop,
         Dreamweaver</p>
+                </div>
+            </section>
+            <section className="skillsTwo">
+                <div>
+                    <h2>Soft Skills</h2>
+                    <p>team player, problem solving, creativity, honest, research, loyal,
+                    adaptable,
+        multitasking, trainability</p>
+                </div>
             </section>
         </div>
     )
