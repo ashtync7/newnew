cb2f1f03663e80d1c4316c033da1f91d7a31dbc0
Ashtyn Ce <ashtyncza@gmail.com>
2021-06-04 23:40:32 -0400

    honestly tho lol sure

---
+++ my-react-app/src/Pros.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Pros(props) {
+
+    return (
+        <div>
+            <section>
+                <h2>Technical Skills</h2>
+                <p>JavaScript, React.js, Express.js, Node.js, HTML, CSS, SCSS, MongoDB,
+                AJAX, jQuery, Bootstrap, Git, GitHub, InDesign, Illustrator, Photoshop,
+        Dreamweaver</p>
+            </section>
+        </div>
+    )
+}
+
+export default Pros;
