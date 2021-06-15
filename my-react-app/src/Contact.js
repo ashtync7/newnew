067593f85f7d697899e65b8d947d219e895547af
Ashtyn Ce <ashtyncza@gmail.com>
2021-06-14 23:43:05 -0400

    pink ladies

--- my-react-app/src/Contact.js
+++ my-react-app/src/Contact.js
@@ -3,13 +3,20 @@ import React from 'react';
 function Contact(props) {
 
     return (
-        <div id="formStuff">
-            <form name="contact">
-                <input type="text" id="cheeseburgers" name="name" className="input1" placeholder="Name" name="Name"></input>
-                <input type="text" id="cheeseburgers" className="input2" placeholder="E-mail Address" name="Email Address"></input>
-                <input type="text" id="formMessage" name="place" rows="3" placeholder="Message" name="Message"></input>
-                <input name="name" type="submit" value="Send" class="sendButton" id="formButton"></input>
-            </form>
+        <div>
+            <div id="formStuff">
+                <form name="contact">
+                    <input type="text" id="cheeseburgers" name="name" className="input1" placeholder="Name" name="Name"></input>
+                    <input type="text" id="cheeseburgers" className="input2" placeholder="E-mail Address" name="Email Address"></input>
+                    <input type="text" id="formMessage" name="place" rows="3" placeholder="Message" name="Message"></input>
+                    <input name="name" type="submit" value="Send" class="sendButton" id="formButton"></input>
+                </form>
+            </div>
+            <div id="socialLinks">
+                <a href="https://www.linkedin.com/in/ashtyncza/" clasName="fa fa-linkedin"></a>
+                <a href="https://www.instagram.com/ash10csky/" className="fa fa-instagram"></a>
+                <a href="https://github.com/ashtync7" className="fa fa-github" aria-hidden="true"></a>
+            </div>
         </div>
     )
 
